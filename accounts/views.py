50cd6b8c340d4e6f990dd129163c527fb5f9fd94
Marco Almeida <simply.marcoalmeida@gmail.com>
2020-10-23 22:06:15 -0300

    criado as views
    criado as views interessado e processo

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home")
+
+def interessado(request):
+    return HttpResponse("Interessado")
+
+def processo(request):
+    return HttpResponse("Processo")
